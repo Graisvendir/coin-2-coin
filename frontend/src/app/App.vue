097cb78d3b7d566a64f85d757ea886bc3ead37cc
jeker96 <jekerr96@gmail.com>
2024-12-10 23:58:33 +0700

    * fix app

--- frontend/src/app/App.vue
+++ frontend/src/app/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { HelloWorld } from '../pages/HelloWorld'
+    import { HelloWorld } from '../pages/HelloWorld';
 </script>
 
 <template>
     <div>
         <a href="https://vite.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
+            <img src="/vite.svg" class="logo" alt="Vite logo">
         </a>
         <a href="https://vuejs.org/" target="_blank">
-            <img src="assets/vue.svg" class="logo vue" alt="Vue logo" />
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo">
         </a>
     </div>
     <HelloWorld msg="Vite + Vue" />
