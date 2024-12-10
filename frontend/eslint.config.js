eb40fe8751a8f922d64d11bfbdf2c6c6ffc87d55
jeker96 <jekerr96@gmail.com>
2024-12-10 22:35:34 +0700

    * больше правил для eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,4 @@
 import eslint from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginVue from 'eslint-plugin-vue';
 import globals from 'globals';
 import typescriptEslint from 'typescript-eslint';
@@ -23,7 +22,109 @@ export default typescriptEslint.config(
         },
         rules: {
             // your rules
+            "vue/script-indent": ["error", 4, {
+                baseIndent: 1,
+                switchCase: 1,
+                ignores: [],
+            }],
+            "vue/html-quotes": ["error", "double", {
+                avoidEscape: false,
+            }],
+
+            "vue/max-attributes-per-line": ["error", {
+                singleline: {
+                    max: 4,
+                },
+
+                multiline: {
+                    max: 1,
+                },
+            }],
+
+            "vue/html-indent": ["error", 4, {
+                attribute: 1,
+                baseIndent: 1,
+                closeBracket: 0,
+                alignAttributesVertically: true,
+                ignores: [],
+            }],
+
+            "vue/no-v-for-template-key-on-child": "error",
+            "vue/no-reserved-keys": "error",
+            "vue/require-v-for-key": "error",
+            "vue/no-mutating-props": "error",
+            "vue/require-component-is": "error",
+            "vue/valid-v-bind": "error",
+            "vue/valid-v-for": "error",
+            "vue/valid-v-if": "error",
+            "vue/valid-v-model": "error",
+            "vue/valid-v-else-if": "error",
+
+            "vue/first-attribute-linebreak": ["error", {
+                singleline: "ignore",
+                multiline: "below",
+            }],
+
+            "max-lines": ["error", 1000],
+            "no-var": 1,
+            "no-eval": "error",
+
+            indent: ["error", 4, {
+                SwitchCase: 1,
+            }],
+
+            quotes: ["error", "single"],
+
+            "space-before-function-paren": ["error", {
+                anonymous: "never",
+                named: "never",
+                asyncArrow: "always",
+            }],
+
+            "no-shadow-restricted-names": "error",
+
+            "padding-line-between-statements": ["error", {
+                blankLine: "always",
+                prev: "*",
+                next: "return",
+            }, {
+                blankLine: "any",
+                prev: ["const", "let"],
+                next: "*",
+            }, {
+                blankLine: "any",
+                prev: ["const", "let"],
+                next: ["const", "let"],
+            }, {
+                blankLine: "any",
+                prev: "*",
+                next: ["if", "for", "do", "while"],
+            }],
+
+            "keyword-spacing": ["error", {
+                before: true,
+            }],
+
+            "comma-dangle": ["error", "always-multiline"],
+            "default-case": "error",
+
+            "no-use-before-define": ["error", {
+                functions: true,
+                classes: true,
+                variables: false,
+            }],
+
+            "max-nested-callbacks": ["error", 3],
+            "no-shadow": "off",
+            "@typescript-eslint/no-shadow": "error",
+            semi: "error",
+
         },
     },
-    eslintConfigPrettier
+    {
+        files: ["**/*.vue"],
+        rules: {
+            indent: "off",
+        },
+    },
 );
