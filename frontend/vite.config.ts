b4f013173b7e5b981d16b4d0eea4c55ba074a31f
merikak <merikak@yandex.ru>
2024-12-10 21:51:29 +0700

    * во фронте структура для FSD

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
