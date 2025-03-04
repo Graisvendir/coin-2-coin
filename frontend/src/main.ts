772049d312b864cb6a6dfbd7760d5556c7a8645e
dmitriy.lashko <dmitriy.lashko@sibirix.ru>
2025-03-04 17:11:32 +0700

    Добавил стор pinia

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue';
 import './style.css';
 import App from '~/app/App.vue';
 import {router} from './app/router.ts';
+import {piniaStore} from './app/store.ts';
 
 createApp(App)
     .use(router)
+    .use(piniaStore)
     .mount('#app');
