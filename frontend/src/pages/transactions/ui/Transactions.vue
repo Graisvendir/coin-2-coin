5dcbef901cb47ed91987ca2e32d9e8e4b94d525f
dmitriy.lashko <dmitriy.lashko@sibirix.ru>
2025-04-04 18:06:36 +0700

    Кнопка добавления транзакции теперь плавает снизу

--- frontend/src/pages/transactions/ui/Transactions.vue
+++ frontend/src/pages/transactions/ui/Transactions.vue
@@ -1,9 +1,12 @@
 <template>
-    <div>
+    <div class="transactions-page">
         <h2>История транзакций</h2>
-        <AddTransactionButton />
 
         <TransactionsHistory />
+
+        <div class="transactions-page__float-button">
+            <AddTransactionButton />
+        </div>
     </div>
 </template>
 
@@ -13,3 +16,19 @@
     import { AddTransactionButton } from '~/features/edit-transaction';
 
 </script>
+
+<style>
+.transactions-page {
+    position: relative;
+    max-width: 60rem;
+}
+
+.transactions-page__float-button {
+    display: flex;
+    justify-content: flex-end;
+    position: sticky;
+    right: 1rem;
+    bottom: 3rem;
+}
+
+</style>
